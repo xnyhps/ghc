3d095e3551917ee0a82d69aa08ecbbd9fc2450f2
Max Bolingbroke <batterseapower@hotmail.com>
2011-06-29 11:06:44 +0100

    Cleanup code outside supercompiler directory

--- compiler/main/GHC.hs
+++ compiler/main/GHC.hs
@@ -244,8 +244,6 @@ import BreakArray
 import InteractiveEval
 #endif
 
-import Supercompile () -- FIXME: remove this. Just used to pull it in
-
 import HscMain
 import GhcMake
 import DriverPipeline	( compile' )
