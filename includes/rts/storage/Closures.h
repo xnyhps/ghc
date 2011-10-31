08ba8f79c36d18598cd966b1f70c20575087fb32
Geoffrey Mainland <gmainlan@microsoft.com>
2011-10-31 13:57:09 +0000

    Make sure the allocator aligns memory on a 16-byte boundary.

--- includes/rts/storage/Closures.h
+++ includes/rts/storage/Closures.h
@@ -138,6 +138,8 @@ typedef struct StgBlockingQueue_ {
 typedef struct {
     StgHeader  header;
     StgWord    bytes;
+    StgWord    dummy1;
+    StgWord    dummy2;
     StgWord    payload[FLEXIBLE_ARRAY];
 } StgArrWords;
 
